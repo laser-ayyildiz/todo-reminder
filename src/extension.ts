92b8d6b39f7ddd7d0671a16dbaeb5efaae8913cd
laser.ayyildiz <laser.ayyildiz@trendyol.com>
2022-09-11 00:12:07 +0300

    comment palette added

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,64 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+"use strict";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "todo-reminder" is now active!');
+import * as vscode from "vscode";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('todo-reminder.remindMe', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from todo-reminder!');
-		
-	});
+let commentId = 1000;
 
-	context.subscriptions.push(disposable);
+class NoteComment implements vscode.Comment {
+  id: number;
+  label: string | undefined;
+  savedBody: string | vscode.MarkdownString; // for the Cancel button
+  constructor(
+    public body: string | vscode.MarkdownString,
+    public mode: vscode.CommentMode,
+    public author: vscode.CommentAuthorInformation,
+  ) {
+    this.id = ++commentId;
+    this.savedBody = this.body;
+  }
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+  // A `CommentController` is able to provide comments for documents.
+  const commentController = vscode.comments.createCommentController(
+    "todo-reminder",
+    "TODO Reminder"
+  );
+  context.subscriptions.push(commentController);
+
+  commentController.commentingRangeProvider = {
+    provideCommentingRanges: (
+      document: vscode.TextDocument,
+      token: vscode.CancellationToken
+    ) => {
+      const lineCount = document.lineCount;
+      return [new vscode.Range(0, 0, lineCount - 1, 0)];
+    },
+  };
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "todo-reminder.remindMe",
+      (reply: vscode.CommentReply) => {
+        replyNote(reply);
+        reply.thread.canReply = false;
+      }
+    )
+  );
+
+  const getUser = async () => {
+    return "todo-reminder";
+  };
+
+  const replyNote = async (reply: vscode.CommentReply) => {
+    const thread = reply.thread;
+    const user = await getUser();
+    const newReminder = new NoteComment(
+      reply.text,
+      vscode.CommentMode.Preview,
+      { name: user },
+    );
+
+    thread.comments = [newReminder];
+  };
+}
